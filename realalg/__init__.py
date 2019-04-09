fc9b3a71ef95f870576c18422efb8c6e77f4c818
Mark Bell <mark00bell@googlemail.com>
2019-04-10 00:56:49 +0100

    Lint.

--- realalg/__init__.py
+++ realalg/__init__.py
@@ -1,8 +1,8 @@
 
 ''' A package for manipulating real algebraic numbers. '''
 
-from .algebraic import RealNumberField, RealAlgebraic  # noqa: F401
-
 import pkg_resources
 __version__ = pkg_resources.get_distribution('realalg').version
+
+from .algebraic import RealNumberField, RealAlgebraic  # noqa: F401
 
