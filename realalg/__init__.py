2d2b30610be82e7eb4c75547399a98eb27cee82d
Mark Bell <mark00bell@googlemail.com>
2019-04-10 00:52:20 +0100

    Added __version__.

--- realalg/__init__.py
+++ realalg/__init__.py
@@ -2,5 +2,7 @@
 ''' A package for manipulating real algebraic numbers. '''
 
 from .algebraic import RealNumberField, RealAlgebraic  # noqa: F401
-# from .interval import Interval  # noqa: F401
+
+import pkg_resources
+__version__ = pkg_resources.get_distribution('realalg').version
 
