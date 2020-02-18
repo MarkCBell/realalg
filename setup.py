54cf7f0bbe5f240754ba1ab5cf9f4dc64fbbc7a7
Mark Bell <mark00bell@googlemail.com>
2020-02-18 22:52:30 +0000

    Corrected version number.

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def readme():
 
 setup(
     name='realalg',
-    version='0.1.5',
+    version='0.2.0',
     description='For manipulating real algebraic numbers',
     long_description=readme(),
     author='Mark Bell',
